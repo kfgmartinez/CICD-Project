3e69b91812388618934ccd657f1bbe0074fcae56
kmartinez <kfg.martinez@hotmail.com>
2023-09-30 16:41:23 -0600

    update files

---
+++ Kubernetes/batch_deply.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo ---------- starting Script ----------
+echo ---------- Applying Namespace ----------
+kubectl apply -f namespace.yaml
+echo ---------- Applying Secret ----------
+kubectl apply -f secret.yaml
+echo ---------- Applying Deployment ----------
+kubectl apply -f deployment.yaml
+echo ---------- Applying Service ----------
+kubectl apply -f service.yaml
+echo ---------- Summary ----------
+kubectl get all -n kfgmartinez-namespace
+
--- Terraform/EKS_sg/eks_security_group.tf
+++ Terraform/EKS_sg/eks_security_group.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "worker_node_sg" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Public Access Via Port"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0

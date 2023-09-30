7c918392c09aaf20fc51777603c8e18a1b06f84e
kmartinez <kfg.martinez@hotmail.com>
2023-09-30 17:41:59 -0600

    batchdeploy update

--- Kubernetes/batch_deply.sh
+++ Kubernetes/batch_deply.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 echo ---------- starting Script ----------
-cd jenkins/workspace/_trend_multibranch_pipeline_main/Kubernetes/ 
 echo ---------- Applying Namespace ----------
-kubectl apply -f namespace.yaml
+kubectl apply -f jenkins/workspace/_trend_multibranch_pipeline_main/Kubernetes/namespace.yaml
 echo ---------- Applying Secret ----------
-kubectl apply -f secret.yaml
+kubectl apply -f jenkins/workspace/_trend_multibranch_pipeline_main/Kubernetes/secret.yaml
 echo ---------- Applying Deployment ----------
-kubectl apply -f deployment.yaml
+kubectl apply -f jenkins/workspace/_trend_multibranch_pipeline_main/Kubernetes/deployment.yaml
 echo ---------- Applying Service ----------
-kubectl apply -f service.yaml
+kubectl apply -f jenkins/workspace/_trend_multibranch_pipeline_main/Kubernetes/service.yaml
 echo ---------- Summary -----------
 kubectl get all -n kfgmartinez-namespace
 
